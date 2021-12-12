a5c5a4e575e732162c25a6ec11aed470e2eef290
adamzainuri01 <95952488+adamzainuri01@users.noreply.github.com>
2021-12-12 15:46:41 +0700

    Update Streamlit_ProduksiMinyak.py

--- Streamlit_ProduksiMinyak.py
+++ Streamlit_ProduksiMinyak.py
@@ -250,8 +250,8 @@ df_produksikumulatifnol = df5[df5['produksi_kumulatif'.format(
 del df_produksikumulatifnol['produksi_kumulatif'.format(T)]
 del df_produksikumulatifnol['index']
 
-table2 = go.Figure(data=[go.Table(header=dict(values=list(df_produksikumulatifnol.columns), fill_color='#4c837a', line_color='#04253a', font=dict(color='#e1ddbf'), align='left'), cells=dict(
-    values=df_produksikumulatifnol.transpose().values.tolist(), fill_color='#7fada6', line_color='#04253a', font=dict(color='#04253a'), align='left'))])
+table2 = go.Figure(data=[go.Table(header=dict(values=list(df_produksikumulatifnol.columns), fill_color='#4c837a', line_color='#e1ddbf', font=dict(color='#e1ddbf'), align='left'), cells=dict(
+    values=df_produksikumulatifnol.transpose().values.tolist(), fill_color='#7fada6', line_color='#e1ddbf', font=dict(color='#04253a'), align='left'))])
 table2.update_layout(title_text="Negara yang Tidak Memproduksi Minyak pada Keseluruhan Tahun",
                      title_x=0, margin=dict(l=0, r=10, b=10, t=30), height=1000)
 
